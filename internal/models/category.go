0346d02c8a70cd958f666049de681048db2ae69a
Adida Wisnu Akbara <wisnuaero3@gmail.com>
2024-04-16 22:14:16 +0700

    feat(models) : add category model, update post model

---
+++ internal/models/category.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"github.com/gosimple/slug"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID		uint `gorm:"primarykey"`
+	Name	string `gorm:"unique;not null" json:"name"`
+	Slug 	string `gorm:"unique;not null" json:"slug"`
+	Posts 	[]Post
+}
+
+func (category *Category) BeforeCreate(tx *gorm.DB) (err error) {
+	category.Slug = slug.Make(category.Name)
+
+	return
+}
--- internal/models/post.go
+++ internal/models/post.go
@@ -1,9 +1,10 @@
 package models
 
 type Post struct {
-	ID     uint   `gorm:"primarykey"`
-	Title  string `gorm:"not null" json:"title"`
-	Body   string `gorm:"type:text" json:"body"`
-	UserID uint   `gorm:"foreignkey:UserID" json:"userID"`
-	User   User   `gorm:"foreignkey:UserID"`
+	ID         uint   `gorm:"primarykey"`
+	Title      string `gorm:"not null" json:"title"`
+	Body       string `gorm:"type:text" json:"body"`
+	UserID     uint   `gorm:"foreignkey:UserID" json:"userID"`
+	User       User   `gorm:"foreignkey:UserID"`
+	CategoryID uint   `gorm:"foreignkey:CategoryID" json:"categoryID"`
 }
